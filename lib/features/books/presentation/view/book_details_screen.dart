fd9bf8afc33ff1a993696a64867abfc435e8571f
zeyad ahmed nour <zezoflutter2025@gmail.com>
2026-05-11 16:02:42 +0300

    Refactor BookDetailsScreen to remove BlocProvider and BlocBuilder, simplifying the widget structure

--- lib/features/books/presentation/view/book_details_screen.dart
+++ lib/features/books/presentation/view/book_details_screen.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:library_app/features/books/data/model/api_book_model.dart';
 
-import '../cubit/books_cubit.dart';
 import 'widgets/details_action_bar.dart';
 import 'widgets/details_card.dart';
 import 'widgets/details_header.dart';
@@ -12,43 +10,33 @@ class BookDetailsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider.value(
-      value: context.read<BooksCubit>(),
+    final bookJson =
+        ModalRoute.of(context)!.settings.arguments
+            as Map<String, dynamic>;
 
-      child: BlocBuilder<BooksCubit, BooksState>(
-        builder: (context, state) {
-          final cubit = context.read<BooksCubit>();
+    final book = ApiBookModel.fromJson(bookJson);
 
-          final bookJson =
-              ModalRoute.of(context)!.settings.arguments
-                  as Map<String, dynamic>;
+    return Scaffold(
+      backgroundColor: const Color(0xffF8FAFC),
 
-          final book = ApiBookModel.fromJson(bookJson);
-
-          return Scaffold(
-            backgroundColor: const Color(0xffF8FAFC),
-
-            body: Stack(
+      body: Stack(
+        children: [
+          SingleChildScrollView(
+            child: Column(
               children: [
-                SingleChildScrollView(
-                  child: Column(
-                    children: [
-                      DetailsHeader(
-                        image: book.image,
-                        isFavorite: cubit.isFavorite,
-                        onFavoriteTap: cubit.toggleFavorite,
-                      ),
-
-                      DetailsCard(book: book),
-                    ],
-                  ),
+                DetailsHeader(
+                  image: book.image,
+                  isFavorite: false, // temporary
+                  onFavoriteTap: () {},
                 ),
 
-                const DetailsActionBar(),
+                DetailsCard(book: book),
               ],
             ),
-          );
-        },
+          ),
+
+          const DetailsActionBar(),
+        ],
       ),
     );
   }
