b7a372b089235e2a4db96309fee21400b51b74da
zeyad ahmed nour <zezoflutter2025@gmail.com>
2026-05-11 14:40:20 +0300

    Add Dio dependency and implement API service with error handling

---
+++ lib/core/services/errors/failures.dart
@@ -0,0 +1,47 @@
+import 'package:dio/dio.dart';
+
+abstract class Failure {
+  final String errorMessage;
+
+  Failure(this.errorMessage);
+}
+
+class ServerFailuer extends Failure {
+  ServerFailuer(super.errorMessage);
+
+  factory ServerFailuer.fromDioError(DioException dioError) {
+    switch (dioError.type) {
+      case DioExceptionType.connectionTimeout:
+        return ServerFailuer("connection Time out with APIServer");
+      case DioExceptionType.sendTimeout:
+        return ServerFailuer("Send Time out");
+      case DioExceptionType.receiveTimeout:
+        return ServerFailuer("Receive Time out with APIServer");
+      case DioExceptionType.badCertificate:
+        return ServerFailuer("bad Certificate with APIServer");
+      case DioExceptionType.badResponse:
+        return ServerFailuer.fromResponse(
+          dioError.response!.statusCode!,
+          dioError.response!.data,
+        );
+      case DioExceptionType.cancel:
+         return ServerFailuer("You`r Requst was Cancel");
+      case DioExceptionType.connectionError:
+         return ServerFailuer("connection Error");
+      case DioExceptionType.unknown:
+        return   ServerFailuer("Unknown Error");
+    }
+  }
+
+  factory ServerFailuer.fromResponse(int statusCode, dynamic response) {
+    if (statusCode == 400 || statusCode == 401 || statusCode == 403) {
+      return ServerFailuer(response['error']['message']);
+    } else if (statusCode == 404) {
+      return ServerFailuer("Your Request not Found, Please try later");
+    } else if (statusCode == 500) {
+      return ServerFailuer("Internal Server Error, Please try later");
+    } else {
+      return ServerFailuer("Opps there was an Error, Please try agin");
+    }
+  }
+}
