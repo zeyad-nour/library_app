8a88eb18f99060018177472d72bb741a96fd8f86
zeyad ahmed nour <zezoflutter2025@gmail.com>
2026-05-12 04:11:33 +0300

    Refactor LogoutButton: implement logout confirmation dialog and streamline logout process

--- lib/features/profile/widgets/logout_button.dart
+++ lib/features/profile/widgets/logout_button.dart
@@ -1,26 +1,60 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:library_app/features/auth/data/repo/auth_repo_implement.dart';
 
 class LogoutButton extends StatelessWidget {
   const LogoutButton({super.key});
+
+  void _showLogoutDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: const Text("Logout"),
+          content: const Text("Are you sure you want to logout?"),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: const Text("Cancel"),
+            ),
+            TextButton(
+              onPressed: () async {
+                Navigator.pop(context);
+
+                AuthRepoImpl authRepoImpl =
+                    AuthRepoImpl(FirebaseAuth.instance);
+
+                await authRepoImpl.signOut();
+
+                Navigator.pushNamedAndRemoveUntil(
+                  context,
+                  '/login',
+                  (route) => false,
+                );
+              },
+              child: const Text(
+                "Logout",
+                style: TextStyle(color: Colors.red),
+              ),
+            ),
+          ],
+        );
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return SizedBox(
       width: double.infinity,
       height: 58,
-
       child: ElevatedButton.icon(
-        onPressed: () {
-          Navigator.pushNamedAndRemoveUntil(
-            context,
-            '/login',
-            (route) => false,
-          );
-        },
+        onPressed: () => _showLogoutDialog(context),
 
         style: ElevatedButton.styleFrom(
           backgroundColor: const Color(0xffEF4444),
-
           shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.circular(20),
           ),
@@ -30,7 +64,6 @@ class LogoutButton extends StatelessWidget {
 
         label: const Text(
           "Logout",
-
           style: TextStyle(color: Colors.white, fontSize: 16),
         ),
       ),
